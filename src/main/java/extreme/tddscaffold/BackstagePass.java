5c2d93f369923972ef33148d6e5b72b6f973d286
huangqiru <hqr13104906171@163.com>
2022-04-13 23:40:11 +0800

    完成BackstagePass

---
+++ src/main/java/extreme/tddscaffold/BackstagePass.java
@@ -0,0 +1,28 @@
+package extreme.tddscaffold;
+
+public class BackstagePass extends Goods {
+    public BackstagePass() {
+    }
+
+    public BackstagePass(Integer sellIn, Integer quality) {
+        super(sellIn, quality);
+    }
+
+    @Override
+    protected Integer calculateCurrentQuality(Integer overDays, Integer changeValueOneDay) {
+        int gapDay = super.sellIn - overDays;
+        if (gapDay < 0) {
+            return quality = 0;
+        }
+        if (gapDay < 10 && gapDay >= 5) {
+            quality += (sellIn - 10) * changeValueOneDay + (overDays - (sellIn - 10)) * 2;
+        }
+        if (gapDay < 5) {
+            quality += (sellIn - 10) * changeValueOneDay + (5 * 2) + (overDays - (sellIn - 5)) * 3;
+        }
+
+        adjustQualityLessThan0OrGreaterThan50();
+
+        return quality;
+    }
+}
--- src/test/java/extreme/tddscaffold/GoodsTest.java
+++ src/test/java/extreme/tddscaffold/GoodsTest.java
@@ -9,12 +9,14 @@ public class GoodsTest {
     private Goods goods;
     private AgedBrie agedBrie;
     private Sulfuras sulfuras;
+    private BackstagePass backstagePass;
 
     @BeforeEach
     public void init() {
         goods = new Goods(10, 20);
         agedBrie = new AgedBrie(0, 20);
         sulfuras = new Sulfuras(0, 20);
+        backstagePass = new BackstagePass(20, 20);
     }
 
     @Test
@@ -66,20 +68,69 @@ public class GoodsTest {
     }
 
     @Test
-    void should_return_20_when_calculate_given_AgedBrie_overDay_is_1() {
+    void should_return_20_when_calculate_given_Sulfuras_overDay_is_1() {
         Integer quality = sulfuras.calculateCurrentQuality(1, 0);
         Assertions.assertEquals(20, quality);
     }
 
     @Test
-    void should_return_20_when_calculate_given_AgedBrie_overDay_is_20() {
+    void should_return_20_when_calculate_given_Sulfuras_overDay_is_20() {
         Integer quality = sulfuras.calculateCurrentQuality(20, 0);
         Assertions.assertEquals(20, quality);
     }
+
     @Test
-    void should_return_20_when_calculate_given_AgedBrie_overDay_is_40() {
+    void should_return_20_when_calculate_given_Sulfuras_overDay_is_40() {
         Integer quality = sulfuras.calculateCurrentQuality(40, 0);
         Assertions.assertEquals(20, quality);
+    }
+
+    @Test
+    void should_return_21_when_calculate_given_BackstagePass_overDay_is_1() {
+        Integer quality = backstagePass.calculateCurrentQuality(1, 1);
+        Assertions.assertEquals(21, quality);
+    }
+
+    @Test
+    void should_return_30_when_calculate_given_BackstagePass_overDay_is_10() {
+        Integer quality = backstagePass.calculateCurrentQuality(10, 1);
+        Assertions.assertEquals(30, quality);
+    }
+
+    @Test
+    void should_return_32_when_calculate_given_BackstagePass_overDay_is_11() {
+        Integer quality = backstagePass.calculateCurrentQuality(11, 1);
+        Assertions.assertEquals(32, quality);
+    }
+
+    @Test
+    void should_return_40_when_calculate_given_BackstagePass_overDay_is_15() {
+        Integer quality = backstagePass.calculateCurrentQuality(15, 1);
+        Assertions.assertEquals(40, quality);
+    }
+
+    @Test
+    void should_return_43_when_calculate_given_BackstagePass_overDay_is_16() {
+        Integer quality = backstagePass.calculateCurrentQuality(16, 1);
+        Assertions.assertEquals(43, quality);
+    }
+
+    @Test
+    void should_return_50_when_calculate_given_BackstagePass_overDay_is_20() {
+        Integer quality = backstagePass.calculateCurrentQuality(20, 1);
+        Assertions.assertEquals(50, quality);
+    }
+
+    @Test
+    void should_return_0_when_calculate_given_BackstagePass_overDay_is_21() {
+        Integer quality = backstagePass.calculateCurrentQuality(21, 1);
+        Assertions.assertEquals(0, quality);
+    }
+
+    @Test
+    void should_return_0_when_calculate_given_BackstagePass_overDay_is_50() {
+        Integer quality = backstagePass.calculateCurrentQuality(50, 1);
+        Assertions.assertEquals(0, quality);
     }
 
 }
